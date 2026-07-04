9a595646b9fcb54374c038987369c3ea2f9f4946
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-07-04 17:31:34 +0300

    add inventory file

---
+++ inventory.py
@@ -0,0 +1,29 @@
+inventory = [
+    {
+        'id': 1,
+        'product_name': 'mount kenya Milk',
+        'brands': 'meru central dairy',
+        'price': 60.00,
+        'stock': 49,
+        'barcode': '6161100',
+        'ingredients': '100% pure cows milk'
+    },
+    {
+        'id': 2,
+        'product_name': 'supa loaf Bread',
+        'brands': "mini bakeries limited",
+        'price': 69.00,
+        'stock': 41,
+        'barcode': '6164000136610',
+        'ingredients': ' wheat flour,vitamin B,vegetable fat, water, yeast, salt'
+    },
+    {
+        'id': 3,
+        'product_name': 'kavagara flour',
+        'brands': 'Raha Premium',
+        'price': 110.00,
+        'stock': 175,
+        'barcode': '0792382360628',
+        'ingredients': '100% pure carefully selected maize grains'
+    }
+]
